990b3053ba22de44296330c49d0907f25f1b91fa
sivaneshwararaj <sivaneshwararaj1020@gmail.com>
2020-05-11 13:37:08 +0530

    updated hosts

--- restproject/settings.py
+++ restproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '242%kxc_fi!hftmc#sr)hhqtte)jwf*9us4q56p5_o(j8ct(ce'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = ['sivaneshwararaj.pythonanywhere.com']
 
